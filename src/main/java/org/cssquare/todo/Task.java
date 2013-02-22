4af2429e090f85edd7b510e1cc9cca2597b37540
Nicole Stewart <nicole.deanna@hotmail.com>
2013-02-21 21:26:56 -0400

    created the task file yo

---
+++ src/main/java/org/cssquare/todo/Task.java
@@ -0,0 +1,17 @@
+package org.cssquare.todo;
+
+public class Task {
+	String task;
+
+	public Task (String taskIn){
+		task = taskIn;
+	}
+
+	public String getTask(){
+		return task;
+	}
+
+	public void setTask(String setT){
+		task = setT;
+	}
+}
